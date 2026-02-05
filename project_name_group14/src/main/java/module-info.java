b0ffec77e8143937e5ae5ccf869eec9697f05c85
Md Maruf <129549328+Maruf195@users.noreply.github.com>
2026-02-05 21:20:14 +0600

    create our project

---
+++ project_name_group14/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example.project_name_group14 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.example.project_name_group14 to javafx.fxml;
+    exports org.example.project_name_group14;
+}
